c51d1f4b9fb7b3c23099b721576a34d8f4155208
youngsun <sunnyskylove22@gmail.com>
2024-01-31 14:30:33 +0900

    [test] 프로그램 test

---
+++ src/main/java/com/ohgiraffers/test/TestApplication.java
@@ -0,0 +1,20 @@
+package com.ohgiraffers.test;
+
+public class TestApplication {
+//수업목표: 오늘의 수업목표입니다.
+    
+   //  여기는 주석입니다.
+    /* 여기는
+    * 여러줄
+    * 주석입니다.
+     */
+
+    public static void main(String[] args) {
+
+        System.out.println("저는 개발자 입니다.");
+
+
+
+    }
+
+}
